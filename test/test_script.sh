d884f9679fdcacea549275df9bcbe0a19ad46d1f
Shardy153 <45224524+Shardy153@users.noreply.github.com>
2018-12-03 11:51:03 +0530

    Committing test case

---
+++ test/test_script.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+bash ../roles/deploy/files/counter < input.txt > output.txt &
+#Storing PID of child process
+PID=$!
+sleep 0.5
+echo "PID of child process: $PID"
+echo "Killing Process...!!"
+kill -15 $PID
+echo "Comparing current output with the known output using diff command."
+printf "\033[01;36mEXPECTED OUTPUT:\n\033[00m"
+cat expected_output.txt
+printf "\033[01;36mCURRENT OUTPUT:\n\033[00m"
+cat output.txt
+diff output.txt expected_output.txt
+if [ $? -eq 0 ]
+then
+  printf "\033[01;32mTEST CASE PASSED.\n\033[00m"
+else
+  printf "\033[01;31mTEST CASE FAILED.\n\033[00m"
+fi
